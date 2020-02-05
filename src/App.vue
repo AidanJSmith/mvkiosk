3bcd4d80fa8debd619b38316b7f2dfff21d8c74f
Aidan Smith <lolbot55555@gmail.com>
2020-02-04 22:15:46 -0800

    First vuwu

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopNav msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopNav from './components/TopNav.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TopNav
   }
 }
 </script>
@@ -24,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <mdb-navbar color="sunny-morning-gradient" class=" py-4">
+    <mdb-navbar-nav nav fill class="d-flex">
+      <mdb-nav-item class="">
+        <mdb-col>
+             <mdb-row style="font-size:small">
+              Time, eventually. Maybe other stuff. Code by Aidan Smith.
+             </mdb-row>
+             <mdb-row style="font-size:1.5em">
+             Welcome to MVHS, it's X degrees out today.
+             </mdb-row>
+        </mdb-col>
+        </mdb-nav-item>
+       </mdb-navbar-nav>
+     <mdb-navbar-nav nav right class="d-flex">
+      <mdb-nav-item class="">
+        <mdb-icon icon="map" size="3x" />
+      </mdb-nav-item>
+     </mdb-navbar-nav>
+   
+  </mdb-navbar>
+</template>
+
+<script>
+export default {
+  name: 'TopNav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
