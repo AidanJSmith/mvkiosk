8d186b54960fda0b8401b2dc25c8a571b63fec57
Kyle <sdbagel@gmail.com>
2020-08-15 17:23:59 -0700

    App launch animation prototype

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <div class="appContainer" :data-app-name="name">
+    <div class="appContainer" :data-app-name="name" v-on:click="route()">
       <div class="appCustomContent">
         <slot></slot>
       </div>
-      <div class="appRouterAnimation collapsed"></div>
     </div>
     <div class="name">
       {{ Name }}
     </div>
+    <div ref="animation" class="launchAnimation" v-bind:style="{ transform: translate }" v-on:click="route()"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Icon",
+  data: function() {
+    return {
+      translate: ""
+    }
+  },
   props: {
     Name: {
       type: String,
@@ -23,6 +28,19 @@ export default {
     Action: {
       type: String,
       default: "App"
+    }
+  },
+  mounted: function() {
+    // Transform the animation to center on the icon
+    const thisRect = this.$el.getBoundingClientRect();
+    const centerX = Math.floor(thisRect.top + thisRect.height / 2);
+    const centerY = Math.floor(thisRect.left + thisRect.width / 2);
+    this.translate = `translate(${centerY}px, ${centerX}px)`;
+  },
+  methods: {
+    // Expands the animation
+    route: function () {
+      this.$refs.animation.classList.toggle("expanded");
     }
   }
 };
@@ -53,31 +71,30 @@ export default {
       width: 100%;
     }
   }
-
-  .appRouterAnimation {
-    background-color: #000;
-    position: absolute;
-    top: -100vmax;
-    bottom: -100vmax;
-    left: -100vmax;
-    right: -100vmax;
-    z-index: 10;
-    width: 300vmax;
-    height: 300vmax;
-    margin: auto;
-    transform-origin: center;
-    transition-duration: 1s;
-
-    &.collapsed {
-      border-radius: 300%;
-      width: 0;
-      height: 0;
-    }
-  }
 }
 
 .name {
   text-align: center;
   margin-top:10px;
 }
+
+.launchAnimation {
+  background-color: #0003;
+  border-radius: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  width: 0;
+  height: 0;
+  transition-duration: 0.4s;
+
+  &.expanded {
+    border-radius: 0;
+    width: 100vmax;
+    height: 100vmax;
+    transform: none !important;
+  }
+}
+
 </style>
