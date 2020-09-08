c61800fb6d642b7480b9e75f4fcb32980f36b216
Aidan Smith <aidanjsmith0@gmail.com>
2020-09-07 18:31:00 -0700

    corny animation

--- src/views/weather/Weather.icon.vue
+++ src/views/weather/Weather.icon.vue
@@ -1,12 +1,33 @@
 <template>
-  <Icon name="Weather" class="x1x2">
-    <div style="display:flex;width:100%;height:100%;align-items:center;justify-content:space-between;background-color:lightblue;">
+  <Icon
+    :name="'Weather'"
+    class="x1x2"
+  >
+    <div :class="['screen', 'screen1',before==1 ? 'before' : 'after']">
       <div style="margin-left:2rem;background-color:lightyellow;height:4rem;width:4rem;border-radius:50%;flex-shrink:0;margin-right:1rem;" />
       <div>
-        Sunny.
+        Sunny
       </div>
       <div style="margin-right:2rem;font-size:2em;font-weight:bold;margin-left:auto;">
-        102°
+        {{ weather.weather }}
+      </div>
+    </div>
+    <div :class="['screen', 'screen2',before==2 ? 'before' : 'after']">
+      <div style="margin-left:2rem;background-color:lightyellow;height:4rem;width:4rem;border-radius:50%;flex-shrink:0;margin-right:1rem;" />
+      <div>
+        Smokey
+      </div>
+      <div style="margin-right:2rem;font-size:2em;font-weight:bold;margin-left:auto;">
+        {{ 20 }}
+      </div>
+    </div>
+    <div :class="['screen', 'screen3',before==3 ? 'before' : 'after']">
+      <div style="margin-left:2rem;background-color:lightyellow;height:4rem;width:4rem;border-radius:50%;flex-shrink:0;margin-right:1rem;" />
+      <div>
+        Cloudy
+      </div>
+      <div style="margin-right:2rem;font-size:2em;font-weight:bold;margin-left:auto;">
+        {{ 20 }}
       </div>
     </div>
   </Icon>
@@ -14,15 +35,75 @@
 
 <script>
   import Icon from "@/components/Icon";
-
+  import WeatherService from "@/views/weather/Weather.service.js"
+  
   export default {
     name: 'WeatherIcon',
     components: {
       Icon
-    }
+    },data() {
+      return {
+        weather: new WeatherService(),
+        before: 1,
+      }
+    },
+    mounted () {
+      setInterval(()=>{
+        if (this.before==3) {
+          this.before=0;
+        }
+        this.before+=1;
+        
+        },4000);
+    },
   }
 </script>
 
 <style lang="scss" scoped>
 
+.x1x2 {
+  border-radius: 20px;
+}
+
+.screen {
+  position: absolute;
+  display:flex;
+  width:100%;
+  height:100%;
+  align-items:center;
+  justify-content:space-between;
+  transition: 2s all;
+  transition-timing-function: ease-in-out;
+  border-radius:20px;
+}
+
+.screen1 {
+  background-color:darkcyan;
+  &.before {
+    left: 0%;
+  }
+  &.after {
+    left: 100%;
+  }
+}
+
+.screen2 {
+  background-color:lightblue;
+  &.before {
+    top:0;
+  }
+  &.after {
+    top: -100%;
+  }
+}
+
+.screen3 {
+  background-color:turquoise;
+  &.before {
+    left:0;
+  }
+  &.after {
+    left:-100%;
+  }
+}
 </style>
--- src/views/weather/Weather.service.js
+++ src/views/weather/Weather.service.js
@@ -1,7 +1,16 @@
 
-export class WeatherService {
-  constructor() {}
-
+export default class WeatherService {
+  constructor() {
+    this.weather={data: "LOADING"};
+    this.updateWeather();
+  }
+  async updateWeather() {
+    this.weather=10;
+    // fetch('https://api.openweathermap.org/data/2.5/weather?q=MountainView&appid=2c7a2dff5dba8638f6700dc8c15cab25').then( data => {
+    //   console.log(data);
+    //   this.weather=data;
+    // });
+  }
   getWeatherJSON() {
     return "";
   }
