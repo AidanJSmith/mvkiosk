57c08ba333e87e2a71639fbe54984ad5f31021d6
Kyle <sdbagel@gmail.com>
2020-12-16 19:31:46 -0800

    Icons: Smoother page transition animation

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -8,12 +8,9 @@
     <div class="name">
       {{ Name }}
     </div>
-    <div
-      ref="animation"
-      class="launchAnimation"
-      :style="{ transform: translate }"
-      @click="route()"
-    />
+    <div ref="animation" class="animationContainer" @click="route()">
+      <div class="animation"></div>
+    </div>
   </div>
 </template>
 
@@ -32,16 +29,8 @@ export default {
   },
   data: function () {
     return {
-      translate: "",
       undoRoute: false,
     };
-  },
-  mounted: function () {
-    // Transform the animation to center on the icon
-    const thisRect = this.$el.getBoundingClientRect();
-    const centerX = Math.floor(thisRect.top + thisRect.height / 2);
-    const centerY = Math.floor(thisRect.left + thisRect.width / 2);
-    this.translate = `translate(${centerY}px, ${centerX}px)`;
   },
   methods: {
     // Expands the animation
@@ -57,7 +46,7 @@ export default {
         (() => {
           if (this.undoRoute) this.$router.push({ path: this.Name });
         }).bind(),
-        800
+        600
       );
     },
   },
@@ -100,26 +89,36 @@ export default {
   margin-top: 10px;
 }
 
-.launchAnimation {
-  background-color: #eee;
-  box-shadow: #0002 5px 5px 80px;
-  border-radius: 200%;
-  position: fixed;
+.animationContainer {
+  position: absolute;
   top: 0;
   left: 0;
   z-index: 10;
-  width: 0;
-  height: 0;
-  transition-duration: 0s;
+  transition-duration: 0.5s;
+  overflow: hidden;
+
+  .animation {
+    background-color: #aaa;
+    border-radius: 300%;
+    display: block;
+    width: 0px;
+    height: 0px;
+    transition-duration: 0.5s;
+  }
 
   &.expanded {
-    background-color: #fff;
-    border-radius: 0;
-    width: 100vmax;
-    height: 100vmax;
-    transform: none !important;
-    transition-duration: 0.5s;
-    transition-timing-function: cubic-bezier(0.5, 1, 0.89, 1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    right: 0;
+    bottom: 0;
+
+    .animation {
+      background-color: #fff;
+      border-radius: 0;
+      width: 100vmax;
+      height: 100vmax;
+    }
   }
 }
 </style>
